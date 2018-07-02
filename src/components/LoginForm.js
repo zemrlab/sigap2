dbedcd7acb5bd5f0716208a6e6006fc1be1e478f
Jafeth Díaz <jafeth.diazc@gmail.com>
2018-07-02 00:10:51 -0500

    refactored rol in login payload

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -14,90 +14,108 @@ const roleSelectOptions = _.values(roles).map(role => ({
   text: v.titleCase(role)
 }));
 
-const LoginForm = props => {
-  const {
-    handleSubmit, 
-    handleChange, 
-    handleBlur, 
-    setFieldValue,
-    isSubmitting, 
-    values,
-    isValid,
-    touched,
-    errors
-  } = props;
+class LoginForm extends React.Component {
+  state = {
+    roles: []
+  }
 
-  return (
-    <div className='login-form'>
-      <style>{`
-        body > div,
-        body > div > div,
-        body > div > div > div.login-form {
-          height: 100%;
-        }
-      `}</style>
-      <Grid
-        textAlign='center'
-        style={{ height: '100%' }}
-        verticalAlign='middle'
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Image src={logo} size="medium" centered/>
-          <Form size='large' onSubmit={handleSubmit} style={{marginTop: 20}}>
-            <Segment stacked>
-              <Form.Select 
-                fluid 
-                options={roleSelectOptions}
-                placeholder='Tipo de usuario'
-                name="rol"
-                value={values.rol}
-                onChange={(e,{name, value}) => setFieldValue(name, value)}
-              />
-              <Form.Input
-                fluid
-                icon='user'
-                iconPosition='left'
-                placeholder='Correo electrónico'
-                name="email"
-                type="text"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched['email'] && !!errors['email']}
-              />
-              <Form.Input
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Contraseña'
-                name="password"
-                type='password'
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched['password'] && !!errors['password']}
-              />
+  componentDidMount(){
+    fetch('http://back-estadisticas.herokuapp.com/LoginController/modulos').then()
+    fetch(`${process.env.REACT_APP_API_ROOT}/LoginController/modulos`)
+    .then(response => response.ok ? response.json() : Promise.resolve({result: 'error'}))
+    .then(response => this.setState({
+      roles: response.map(({modulo}) => ({
+        key: modulo,
+        value: modulo,
+        text: v.titleCase(modulo)
+      }))
+    }))
+  }
 
-              {errors._error && (
-                <Message color="red">{errors._error}</Message>
-              )}
+  render(){
+    const {
+      handleSubmit, 
+      handleChange, 
+      handleBlur, 
+      setFieldValue,
+      isSubmitting, 
+      values,
+      isValid,
+      touched,
+      errors
+    } = this.props;
+    return (
+      <div className='login-form'>
+        <style>{`
+          body > div,
+          body > div > div,
+          body > div > div > div.login-form {
+            height: 100%;
+          }
+        `}</style>
+        <Grid
+          textAlign='center'
+          style={{ height: '100%' }}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Image src={logo} size="medium" centered/>
+            <Form size='large' onSubmit={handleSubmit} style={{marginTop: 20}}>
+              <Segment stacked>
+                <Form.Select 
+                  fluid 
+                  options={this.state.roles}
+                  placeholder='Tipo de usuario'
+                  name="rol"
+                  value={values.rol}
+                  onChange={(e,{name, value}) => setFieldValue(name, value)}
+                />
+                <Form.Input
+                  fluid
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='Correo electrónico'
+                  name="email"
+                  type="text"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched['email'] && !!errors['email']}
+                />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Contraseña'
+                  name="password"
+                  type='password'
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={touched['password'] && !!errors['password']}
+                />
   
-              <Button color='teal' fluid size='large' disabled={isSubmitting || !isValid}>
-                Ingresar&nbsp;
-                <Loader active={isSubmitting} size="tiny" inline/>
-              </Button>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    </div>
-  );
-} 
-
+                {errors._error && (
+                  <Message color="red">{errors._error}</Message>
+                )}
+    
+                <Button color='teal' fluid size='large' disabled={isSubmitting || !isValid}>
+                  Ingresar&nbsp;
+                  <Loader active={isSubmitting} size="tiny" inline/>
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 const validationSchema = yup.object().shape({
   email: yup.string()/* .email('Correo electrónico inválido') */.required('Correo electrónico es requerido'),
-  password: yup.string().min(3, 'Contraseña debe ser más larga.').required('Password requerido')
+  password: yup.string().min(3, 'Contraseña debe ser más larga.').required('Password requerido'),
+  rol: yup.string().required('Rol es requerido')
 });
 
 const mapPropsToValues =  () => ({email: '', password: '', rol: roleSelectOptions[0].value});
--- src/utils/modules.js
+++ src/utils/modules.js
@@ -1,52 +1,43 @@
-import roles from './roles';
-
 export default {
   7: {
     name: 'Legajo Docente',
     url: 'https://fisitcsld.herokuapp.com',
-    allowedRoles: [roles.STUDENT, roles.PROFESSOR, roles.ADMIN],
     icon: "user"
   },
 
   1: {
     name: 'Carga de Recaudaciones',
     url: 'http://modulodecarga.herokuapp.com',
-    allowedRoles: [roles.ADMIN],
     icon: "piggy-bank"
   },
 
   5: {
     name: 'Disponibilidad Docente',
     url: 'http://disponibdocente.herokuapp.com',
-    allowedRoles: [roles.PROFESSOR, roles.ADMIN],
     icon: "calendar"
   },
 
   3: {
     name: 'Estadísticas',
     url: 'https://xbaxtian.github.io/front-estadisticas-sigap',
-    allowedRoles: [roles.ADMIN],
     icon: "stats"
   },
 
   6: {
     name: 'Control Tesistas',
     url: 'https://control-tesis-app.herokuapp.com',
-    allowedRoles: [roles.STUDENT, roles.PROFESSOR, roles.ADMIN],
     icon: "education"
   },
 
   2: {
     name: 'Control de Recibos',
     url: 'https://modulo-control-recibos.herokuapp.com',
-    allowedRoles: [roles.ADMIN],
     icon: "list-alt"
   },
 
   4: {
     name: 'Estado de Pagos',
     url: 'http://modulo-alumno-ui.herokuapp.com',
-    allowedRoles: [roles.ADMIN],
     icon: "usd"
   },
 };
