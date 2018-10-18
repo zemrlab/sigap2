eccb3a3ffff518b5d0808593ef2bad39c29027f1
Juan Araujo <juane.araujob@gmail.com>
2018-10-18 15:59:03 -0500

    Add chechkbox

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -87,7 +87,7 @@ class LoginForm extends React.Component {
                   onBlur={handleBlur}
                   error={touched['password'] && !!errors['password']}
                 />
-  
+                <div><Form.Input type="checkbox" name="recuperar" value="si"/>Recuperar contraseña</div>
                 {errors._error && (
                   <Message color="red">{errors._error}</Message>
                 )}
